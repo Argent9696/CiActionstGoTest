7d8c6a926e944f045962a58594e70e76f1a89849
nabos.dezigner <nabos.dezigner@gmail.com>
2024-08-28 18:36:38 +0300

    Тест

---
+++ example/main.go
@@ -0,0 +1,15 @@
+File: main.go
+package main
+
+import "fmt"
+
+func "fmt"
+
+func main()  {
+    msg := sayHello("Nabos")
+    fmt.Println(msg)
+}
+
+func sayHello(name string) string {
+    return fmt.sprintf("Hi %s, name)
+}
